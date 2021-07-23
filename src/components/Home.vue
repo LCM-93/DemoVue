89201d17708e3b93a3d00b84ae7c8af0c20074a8
lichenming <lichenming@shengqugames.com>
2021-07-23 15:54:19 +0800

    .

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,8 +1,18 @@
 <template>
   <el-container>
-    <el-header>Header</el-header>
+    <!-- 头部区域 -->
+    <el-header>
+      <div>
+        <img src="../assets/logo.png" />
+        <span>电商管理后台</span>
+      </div>
+      <el-button type="info" @click="logout">退出</el-button>
+    </el-header>
+    <!-- 页面主体区域 -->
     <el-container>
-      <el-aside width="200px">Aside</el-aside>
+      <!-- 侧边栏 -->
+      <el-aside>Aside</el-aside>
+      <!-- 右侧内容主体 -->
       <el-main>Main</el-main>
     </el-container>
   </el-container>
@@ -10,8 +20,45 @@
 
 <script>
 export default {
-
+  methods: {
+    logout () {
+      window.sessionStorage.clear()
+      this.$router.push('/Login')
+    }
+  }
 }
 </script>
 <style lang="less" scoped>
+.el-container {
+  width: 100%;
+  height: 100%;
+}
+.el-header {
+  background-color: #373d41;
+  display: flex;
+  justify-content: space-between;
+  padding-left: 0px;
+  align-items: center;
+  color: #fff;
+  font-size: 20px;
+
+  div {
+    display: flex;
+    align-items: center;
+    img {
+      width: 90px;
+      height: 90px;
+    }
+    span {
+      margin-left: 15px;
+    }
+  }
+}
+.el-aside {
+  width: 300px;
+  background-color: #333744;
+}
+.el-main {
+  background-color: #eaedf1;
+}
 </style>
