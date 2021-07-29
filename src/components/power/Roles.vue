658d7f5ea3fce8da1603fdea670a5492ca5684d2
lichenming <lichenming@shengqugames.com>
2021-07-29 16:53:54 +0800

    .

--- src/components/power/Roles.vue
+++ src/components/power/Roles.vue
@@ -9,7 +9,7 @@
 
     <el-card>
       <el-row>
-        <el-button type="primary" @click="addRoleVisible = true">添加角色</el-button>
+        <el-button type="primary" @click="showAddModifyDialog(true)">添加角色</el-button>
       </el-row>
 
       <el-table :data="rolesList" border stripe>
@@ -39,28 +39,30 @@
         <el-table-column prop="roleName" label="角色名称"></el-table-column>
         <el-table-column prop="roleDesc" label="角色描述"></el-table-column>
         <el-table-column label="操作">
-          <el-button type="primary" icon="el-icon-edit" size="mini"></el-button>
-          <el-button type="danger" icon="el-icon-delete" size="mini"></el-button>
-          <el-tooltip effect="dark" content="分配角色" placement="top" :enterable="false">
-            <el-button type="warning" icon="el-icon-setting" size="mini"></el-button>
-          </el-tooltip>
+          <template v-slot:default="scope">
+            <el-button type="primary" icon="el-icon-edit" size="mini" @click="modifyRole(scope.row.id)"></el-button>
+            <el-button type="danger" icon="el-icon-delete" size="mini" @click="deleteRole(scope.row.id)"></el-button>
+            <el-tooltip effect="dark" content="分配角色" placement="top" :enterable="false">
+              <el-button type="warning" icon="el-icon-setting" size="mini"></el-button>
+            </el-tooltip>
+          </template>
         </el-table-column>
       </el-table>
     </el-card>
 
-    <!-- 添加角色的弹窗 -->
-    <el-dialog title="添加角色" :visible.sync="addRoleVisible" width="50%" @close="addRoleDialogClose">
-      <el-form :model="addRoleForm" :rules="addRoleFormRules" ref="addRoleFormRef" label-width="80px">
+    <!-- 添加修改角色的弹窗 -->
+    <el-dialog :title="isAddRoleMode?'添加角色':'修改角色'" :visible.sync="addModifyRoleVisible" width="50%" @close="addModifyRoleDialogClose">
+      <el-form :model="addModifyForm" :rules="addModifyFormRules" ref="addModifyFormRef" label-width="80px">
         <el-form-item label="角色名称" prop="roleName">
-          <el-input v-model="addRoleForm.roleName"></el-input>
+          <el-input v-model="addModifyForm.roleName"></el-input>
         </el-form-item>
         <el-form-item label="角色描述">
-          <el-input v-model="addRoleForm.roleDesc"></el-input>
+          <el-input v-model="addModifyForm.roleDesc"></el-input>
         </el-form-item>
       </el-form>
       <span slot="footer" class="dialog-footer">
-        <el-button @click="addRoleVisible = false">取 消</el-button>
-        <el-button type="primary" @click="addRole">确 定</el-button>
+        <el-button @click="addModifyRoleVisible = false">取 消</el-button>
+        <el-button type="primary" @click="addModifyRole">确 定</el-button>
       </span>
     </el-dialog>
   </div>
@@ -70,12 +72,13 @@ export default {
   data () {
     return {
       rolesList: [],
-      addRoleVisible: false,
-      addRoleForm: {
+      isAddRoleMode: true,
+      addModifyRoleVisible: false,
+      addModifyForm: {
         roleName: '',
         roleDesc: ''
       },
-      addRoleFormRules: {
+      addModifyFormRules: {
         roleName: [
           { required: true, message: '请输入角色名', trigger: 'blur' }
         ]
@@ -116,23 +119,74 @@ export default {
       }
       role.children = res.data
     },
-    addRoleDialogClose () {
-      this.$refs.addRoleFormRef.resetFields()
+    showAddModifyDialog (isAddMode) {
+      this.isAddRoleMode = isAddMode
+      if (isAddMode) {
+        this.addModifyForm = {
+          roleName: '',
+          roleDesc: ''
+        }
+      }
+      this.addModifyRoleVisible = true
     },
-    addRole () {
-      this.$refs.addRoleFormRef.validate(async valid => {
+    addModifyRoleDialogClose () {
+      this.$refs.addModifyFormRef.resetFields()
+    },
+    addModifyRole () {
+      this.$refs.addModifyFormRef.validate(async valid => {
         console.log('校验结果：' + valid)
         if (!valid) return
-        const { data: res } = await this.$http.post('roles', this.addRoleForm)
-        console.log(res)
-        this.addRoleVisible = false
-        if (res.meta.status !== 201) {
-          return this.$message.error('添加角色失败！')
+        if (this.isAddRoleMode) {
+          const { data: res } = await this.$http.post('roles', this.addModifyForm)
+          console.log(res)
+          this.addModifyRoleVisible = false
+          if (res.meta.status !== 201) {
+            return this.$message.error('添加角色失败！')
+          }
+          this.$message.success('添加角色成功!')
+        } else {
+          const { data: res } = await this.$http.put(`roles/${this.addModifyForm.roleId}`, this.addModifyForm)
+          console.log(res)
+          this.addModifyRoleVisible = false
+          if (res.meta.status !== 200) {
+            return this.$message.error('修改角色失败！')
+          }
+          this.$message.success('修改角色成功!')
         }
-        this.$message.success('添加角色成功!')
-        this.$refs.addRoleFormRef.resetFields()
+        this.$refs.addModifyFormRef.resetFields()
         this.getRolesList()
       })
+    },
+    async modifyRole (roleId) {
+      const { data: res } = await this.$http.get(`roles/${roleId}`)
+      console.log(res)
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取角色信息失败！')
+      }
+      this.addModifyForm = res.data
+      this.showAddModifyDialog(false)
+    },
+    async deleteRole (roleId) {
+      const confirmResult = await this.$confirm(
+        '此操作将永久删除该角色, 是否继续?',
+        '提示',
+        {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).catch(err => err)
+      // 点击确定 返回值为：confirm
+      // 点击取消 返回值为： cancel
+      if (confirmResult !== 'confirm') {
+        return this.$message.info('取消删除')
+      }
+      const { data: res } = await this.$http.delete(
+        `roles/${roleId}`
+      )
+      if (res.meta.status !== 200) {
+        return this.$message.error('删除角色失败！')
+      }
+      this.getRolesList()
     }
   }
 }
